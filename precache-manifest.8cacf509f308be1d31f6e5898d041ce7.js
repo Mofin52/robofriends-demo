7b27a8753c3cd9def5ccc5c0def6a827a27a4e33
Mofin52 <a.velikorechin@gmail.com>
2019-03-27 10:25:53 +0300

    Updates

---
+++ precache-manifest.8cacf509f308be1d31f6e5898d041ce7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-demo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3167531f370c69b08c26",
+    "url": "/robofriends-demo/static/js/runtime~main.fe351747.js"
+  },
+  {
+    "revision": "40963a3f45bb00f57138",
+    "url": "/robofriends-demo/static/js/main.c4c3295e.chunk.js"
+  },
+  {
+    "revision": "ad42e2c08e346c90c016",
+    "url": "/robofriends-demo/static/js/2.96a3a30f.chunk.js"
+  },
+  {
+    "revision": "40963a3f45bb00f57138",
+    "url": "/robofriends-demo/static/css/main.5f2ac6ed.chunk.css"
+  },
+  {
+    "revision": "ad42e2c08e346c90c016",
+    "url": "/robofriends-demo/static/css/2.7e39fb73.chunk.css"
+  },
+  {
+    "revision": "451344e31fdb34757a309b7d0f6e6126",
+    "url": "/robofriends-demo/index.html"
+  }
+];
